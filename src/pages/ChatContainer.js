20934609599817f1e7c830f3aa9fe28a7eb468e5
kulinskir <robert.kulinski93@gmail.com>
2020-06-13 13:06:41 +0200

    Add username to chat.

--- src/pages/ChatContainer.js
+++ src/pages/ChatContainer.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class ChatContainer extends React.Component {
   render() {
     return (
       <div>
         <h1>Chat</h1>
+        <div>{this.props.username}</div>
       </div>
     );
   }
 }
 
-export default ChatContainer;
+const mapStateToProps = (state) => ({
+  username: state.globalReducer.username,
+});
+
+export default connect(mapStateToProps)(ChatContainer);
